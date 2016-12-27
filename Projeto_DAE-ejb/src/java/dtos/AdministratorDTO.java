da910943971d507cf8a2b820f6a714dc9e04523d
DESKTOP-IRDLG5A\Figueiredo <klisarkk@gmail.com>
2016-12-27 13:22:53 +0000

    FIGUEIREDO POWA

--- Projeto_DAE-ejb/src/java/dtos/AdministratorDTO.java
+++ Projeto_DAE-ejb/src/java/dtos/AdministratorDTO.java
@@ -2,52 +2,19 @@ package dtos;
 
 import java.io.Serializable;
 
-public class AdministratorDTO implements Serializable {
+public class AdministratorDTO extends UserDTO implements Serializable {
     
-    private String username;
-    private String name;
-    private String password;
+
 
     public AdministratorDTO() {
     }    
     
     public AdministratorDTO(String username, String name, String password) {
-        this.username = username;
-        this.name = name;
-        this.password= password;
+       super(username, name, password);
         
     }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    
-    public void reset() {
-        setName(null);
-        setUsername(null);
-        setPassword(null);  
-    }    
 
 
 }
